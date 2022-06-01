ccodwg/Covid19CanadaETL

commit 8c7344955bac583cd962edf19b69494a1cdf5df4

Update data source for YT tests
- ccodwg/CovidTimelineCanada#56

R/assemble_final_datasets.R
@@ -412,8 +412,19 @@ assemble_final_datasets <- function() {
 
   # tests_completed dataset
 
-  ## collate and process final dataset
+  ## all regions
   tests_completed_pt <- get_phac_d("tests_completed", "all") %>%
+    dplyr::filter(!.data$region %in% c("YT"))
+
+  ## replace YT
+  tests_completed_pt <- dplyr::bind_rows(
+    tests_completed_pt,
+    read_d("raw_data/active_ts/yt/yt_tests_completed_pt_ts.csv") %>%
+      add_hr_col("Yukon")
+  )
+
+  ## collate and process final dataset
+   tests_completed_pt <- tests_completed_pt %>%
     dataset_format("pt")
 
   # vaccine coverage dataset

R/dl_datasets.R
@@ -45,7 +45,8 @@ dl_datasets <- function() {
       "f0c25e20-2a6c-4f9a-adc3-61b28ab97245", NA, NA, # h/i
       # sk
       # yt
-      "8eb9a22f-a2c0-4bdb-8f6c-ef8134901efe", NA, NA # c
+      "8eb9a22f-a2c0-4bdb-8f6c-ef8134901efe", NA, NA, # c
+      "4a33ab2c-32a4-4630-8f6b-2bac2b1ce7ca", NA, NA # tests_completed
     ),
     ncol = 3,
     byrow = TRUE)

R/update_raw_datasets.R
@@ -174,8 +174,10 @@ update_active_ts <- function(ds) {
     ds = load_ds(ds, "f0c25e20-2a6c-4f9a-adc3-61b28ab97245")) %>%
     write_ts("active_ts", "qc", "icu")
 
-  # testing data
+  # active_ts - testing data
   cat("Updating active_ts: testing data", fill = TRUE)
+
+  ## can
   Covid19CanadaDataProcess::process_dataset(
     uuid = "f7db31d0-6504-4a55-86f7-608664517bdb",
     val = "testing",
@@ -184,6 +186,16 @@ update_active_ts <- function(ds) {
     ds = load_ds(ds, "f7db31d0-6504-4a55-86f7-608664517bdb")) %>%
     add_name_col("tests_completed") %>%
     write_ts("active_ts", "can", "tests_completed")
+
+  ## yt
+  Covid19CanadaDataProcess::process_dataset(
+    uuid = "4a33ab2c-32a4-4630-8f6b-2bac2b1ce7ca",
+    val = "testing",
+    fmt = "prov_ts",
+    testing_type = "n_tests_completed",
+    ds = load_ds(ds, "4a33ab2c-32a4-4630-8f6b-2bac2b1ce7ca")) %>%
+    add_name_col("tests_completed") %>%
+    write_ts("active_ts", "yt", "tests_completed")
 
   # vaccine coverage data
   cat("Updating active_ts: vaccine coverage data", fill = TRUE)

commit 18bb747858b0013699cc510ae4b803195dabf5bf

Update testing data
- Fix regression caused by 8c7344955bac583cd962edf19b69494a1cdf5df4 (remove sub_region_1 column from tests_completed dataset) (ccodwg/CovidTimelineCanada#56)
- Truncate PHAC testing time series by last distinct value for each PT (ccodwg/CovidTimelineCanada#57)

R/assemble_final_datasets.R
@@ -413,14 +413,13 @@ assemble_final_datasets <- function() {
   # tests_completed dataset
 
   ## all regions
-  tests_completed_pt <- get_phac_d("tests_completed", "all") %>%
+  tests_completed_pt <- get_phac_d("tests_completed", "all", keep_up_to_date = TRUE) %>%
     dplyr::filter(!.data$region %in% c("YT"))
 
   ## replace YT
   tests_completed_pt <- dplyr::bind_rows(
     tests_completed_pt,
-    read_d("raw_data/active_ts/yt/yt_tests_completed_pt_ts.csv") %>%
-      add_hr_col("Yukon")
+    read_d("raw_data/active_ts/yt/yt_tests_completed_pt_ts.csv")
   )
 
   ## collate and process final dataset

R/utils.R
@@ -99,8 +99,8 @@ read_d <- function(file, val_numeric = FALSE) {
 #' @param val The value to read data for.
 #' @param region Either "all" or the region to read data for.
 #' @param exclude_repatriated Exclude "Repatriated" region when reading data for "all" regions? Default: TRUE.
-#' @param keep_up_to_date Keep data for each province/territory only up to the date it was most recently updated
-#' (i.e., the most recent date with `update` = 1)? Default: FALSE (i.e., keep all data).
+#' @param keep_up_to_date Keep data for each province/territory only up to the date it was most recently updated?
+#' Method of filtering differs by value. Default: FALSE (i.e., keep all data).
 #' Ignored if there is no `update` column.
 #'
 #' @export
@@ -148,9 +148,7 @@ get_phac_d <- function(val, region, exclude_repatriated = TRUE, keep_up_to_date 
       }
       # filter up to most recent date updated for each P/T
       if (keep_up_to_date) {
-        if (!"update" %in% names(d)) {
-          warning("There is no column 'update', ignoring 'keep_up_to_date = TRUE'...")
-        } else {
+        if (val %in% c("cases", "deaths")) {
           # get unique PTs
           pts <- unique(d$region)
           pts <- pts[!pts %in% c("CAN", "RT")]
@@ -171,11 +169,24 @@ get_phac_d <- function(val, region, exclude_repatriated = TRUE, keep_up_to_date 
               d <- d %>%
                 dplyr::filter(.data$region != pt | .data$date <= update_max_date)
             }
+            # drop 'update' column
+            d <- d[, names(d) != "update"]
           }
+        } else if (val == "tests_completed") {
+          # get unique PTs
+          pts <- unique(d$region)
+          # most recent distinct value is the latest update date
+          for (pt in pts) {
+            max_date <- dplyr::distinct(d[d$region == pt, ], .data$value, .keep_all = TRUE) %>%
+              dplyr::slice_tail(n = 1) %>%
+              dplyr::pull(.data$date)
+            d <- d %>%
+              dplyr::filter(.data$region != pt | .data$date <= max_date)
+          }
+        } else {
+          warning("keep_up_to_date = TRUE is not supported with this value, ignoring...")
         }
       }
-      # drop 'update' column, if it exists
-      d <- d[, names(d) != "update"]
       # return data
       d
     },