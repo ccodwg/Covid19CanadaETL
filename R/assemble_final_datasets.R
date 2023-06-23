035e4cd5a9d7930c5bf6324f0b6be34eb868ae56
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2023-06-22 22:34:03 -0400

    Fix reporting of NS case data
    - See ccodwg/CovidTimelineCanada#107

--- R/assemble_final_datasets.R
+++ R/assemble_final_datasets.R
@@ -79,12 +79,17 @@ assemble_final_datasets <- function() {
     report_pluck("cases", "cases", "value_daily", "hr") %>%
     dplyr::mutate(sub_region_1 = sub("Zone \\d - ", "", .data$sub_region_1))
   cases_ns <- append_daily_d(cases_ns, ns3)
-  ns4 <- read_d("raw_data/static/ns/ns_cases_pt_ts.csv") %>%
+  ns4 <- read_d("raw_data/static/ns/ns_cases_pt_ts_1.csv") %>%
     dplyr::mutate(value_daily = c(dplyr::slice_head(., n = 1) %>% dplyr::pull(.data$value), diff(.data$value))) %>% # convert to daily
-    dplyr::filter(.data$date >= as.Date("2022-03-05")) %>%
+    dplyr::filter(.data$date >= as.Date("2022-03-05") & .data$date < as.Date("2022-07-01")) %>%
     add_hr_col("Unknown")
   cases_ns <- append_daily_d(cases_ns, ns4)
-  rm(ns1, ns2, ns3, ns4) # cleanup
+  ns5 <- read_d("raw_data/static/ns/ns_cases_pt_ts_2.csv") %>%
+    dplyr::mutate(value_daily = c(dplyr::slice_head(., n = 1) %>% dplyr::pull(.data$value), diff(.data$value))) %>% # convert to daily
+    dplyr::filter(.data$date >= as.Date("2022-07-01")) %>%
+    add_hr_col("Unknown")
+  cases_ns <- append_daily_d(cases_ns, ns5)
+  rm(ns1, ns2, ns3, ns4, ns5) # cleanup
 
   ## nt
   cases_nt <- dplyr::bind_rows(
