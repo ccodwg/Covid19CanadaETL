ae3c0a29c44989ba8345bbd19c9465a7ba16a16c
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2023-10-29 14:02:40 -0400

    PT-level data replacement for cases: SK
    - Use PHAC data for SK cases beginning 2022-07-02
    - ccodwg/CovidTimelineCanada#120

--- R/assemble_final_datasets.R
+++ R/assemble_final_datasets.R
@@ -182,18 +182,15 @@ assemble_final_datasets <- function() {
         report_pluck("cases", "cases", "value_daily", "hr") %>%
         dplyr::filter(.data$date > as.Date("2022-02-06")) %>% # overlaps with end of TS
         report_recent()
-      sk3 <- read_d("raw_data/reports/sk/sk_monthly_report.csv") %>%
-        report_pluck("cases", "cases", "value_daily", "hr")
-      sk4 <- read_d("raw_data/reports/sk/sk_crisp_report.csv") %>%
-        report_pluck("cases", "cases", "value_daily", "pt") %>%
-        add_hr_col("Unknown") %>%
-        report_recent()
+      sk3 <- get_phac_d("cases", "SK", keep_up_to_date = TRUE) %>%
+        dplyr::mutate(value_daily = c(dplyr::slice_head(., n = 1) %>% dplyr::pull(.data$value), diff(.data$value))) |>
+        dplyr::filter(.data$date >= as.Date("2022-07-02")) |>
+        add_hr_col("Unknown")
       cases_sk <- append_daily_d(sk1, sk2) %>%
         dplyr::mutate(
           sub_region_1 = ifelse(.data$sub_region_1 == "Not Assigned", "Unknown", .data$sub_region_1))
       cases_sk <- append_daily_d(cases_sk, sk3)
-      cases_sk <- append_daily_d(cases_sk, sk4)
-      rm(sk1, sk2, sk3, sk4) # cleanup
+      rm(sk1, sk2, sk3) # cleanup
     },
     error = function(e) {
       print(e)
